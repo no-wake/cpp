5ae922c9bc8eea26027cd7d1d205dc06e3c82055
no-wake <zym9420@petalmail.com>
2023-06-16 00:45:29 +0800

    改进

--- daily/1-29.cpp
+++ daily/1-29.cpp
@@ -14,6 +14,18 @@ T *copy(T *arr,int end,int to)
     return arr2;
 }
 
+
+template<class T>
+T copy2(T *arr,T *end,T *to)
+{
+    for(;arr!=end;)
+    {
+        *to=*arr;
+        arr++,to++;
+    }
+    return 0;
+}
+
 int main(){
     int arr1[]={1,2,3,4,5};
     for(int i=0;i<=6;i++)
@@ -28,6 +40,15 @@ int main(){
         std::cout<<copy(arr2,6,3)[i]<<' ';
     }
     std::cout<<endl;
+
+
+    int arr3[]={1,2,3,4,5};
+    int arr4[]={3,3,3,3,3};
+    copy2(arr3,arr3+4,arr4);
+    for(int j=0;j<5;j++)
+{
+    std::cout<<arr4[j]<<' ';
+}
 
     return 0;
 
